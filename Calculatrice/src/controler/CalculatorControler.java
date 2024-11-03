5ec449e6613ae83faf54a427f6bd819ab447196b
ArthurWav <arthur.wavreille@etu.mines-ales.fr>
2024-11-03 16:50:42 +0100

    Commit visuel fonctionnel

--- Calculatrice/src/controler/CalculatorControler.java
+++ Calculatrice/src/controler/CalculatorControler.java
@@ -2,8 +2,21 @@ package controler;
 
 import java.util.List;
 
+import model.CalculatorModel;
+import model.CalculatorModelInterface;
+import view.CalculatorGUI;
+import view.CalculatorGUIInterface;
+
 public class CalculatorControler implements CalculatorControlerInterface {
 
+	private CalculatorModelInterface model;
+	private CalculatorGUIInterface gui;
+	
+	public CalculatorControler(CalculatorModelInterface model, CalculatorGUIInterface gui) {
+		this.model = model;
+		this.gui = gui;
+	}
+	
 	@Override
 	public void change(String accu) {
 		// TODO Auto-generated method stub
